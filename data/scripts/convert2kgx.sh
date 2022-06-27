9c35ccaff73964c8e32ba058b028d5de4f8fd187
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-06-27 03:03:09 -0400

    Corrected path.

--- data/scripts/convert2kgx.sh
+++ data/scripts/convert2kgx.sh
@@ -1,3 +1,3 @@
 for FILENAME in ../ontologies/*.json; do
-	poetry run python -m ontorunner.pre.util json2tsv -i ${FILENAME} -o ${FILENAME%.tsv}
+	poetry run python -m ontorunner.pre.util json2tsv -i ${FILENAME} -o ../nodes_and_edges/${FILENAME%.tsv}
 done;
