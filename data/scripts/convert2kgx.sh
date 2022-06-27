4adfa44f0cbdf0e32592e6313fe8333151902519
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-06-27 03:41:50 -0400

    Fixed script.

--- data/scripts/convert2kgx.sh
+++ data/scripts/convert2kgx.sh
@@ -1,3 +1,4 @@
 for FILENAME in ../ontologies/*.json; do
-	poetry run python -m ontorunner.pre.util json2tsv -i ${FILENAME} -o ../nodes_and_edges/${FILENAME%.tsv}
+	BASENAME=$(basename $FILENAME)
+	poetry run python -m ontorunner.pre.util json2tsv -i ${FILENAME} -o ../nodes_and_edges/${BASENAME%.json}
 done;
